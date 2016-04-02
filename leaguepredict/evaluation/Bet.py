8c71f3d900857e75ec163773d668551678be409c
Shane Brennan <brennash@Shanes-MacBook-Pro.local>
2016-04-02 21:35:35 +0100

    Adding the minimum working version

--- leaguepredict/evaluation/Bet.py
+++ leaguepredict/evaluation/Bet.py
@@ -1,8 +1,9 @@
+import datetime
+
 class Bet:
 
-	def __init__(self, type='single'):
+	def __init__(self, result, date, homeTeam, awayTeam, homeFT, awayFT, prediction, threshold, configThreshold, bestOdds, worstOdds):
 		self.resultList = []
-		self.type  		= type
 		self.dateList   = []
 		self.homeTeam   = []
 		self.awayTeam   = []
@@ -13,47 +14,42 @@ class Bet:
 		self.configThreshold = []
 		self.bestOdds   = []
 		self.worstOdds  = []
-		self.win        = False
-		self.ignored    = True
 		
-	def addDetails(self, result, date, homeTeam, awayTeam, homeFT, awayFT, prediction, threshold, configThreshold, bestOdds, worstOdds):
-		if self.type == 'single':
-			self.resultList = [result]
-			self.dateList   = [date]
-			self.homeTeam   = [homeTeam]
-			self.awayTeam   = [awayTeam]
-			self.homeFT     = [homeFT]
-			self.awayFT     = [awayFT]
-			self.prediction = [prediction]
-			self.threshold  = [threshold]
-			self.configThreshold = [configThreshold]
-			self.bestOdds   = [bestOdds]
-			self.worstOdds  = [worstOdds]
-			if threshold >= configThreshold:
-				self.ignored = False
-				if result > 0.0:
-					self.win = True
-				else:
-					self.win = False
-		else:
-				self.resultList = []
-		self.type  		= type
-		self.dateList   = []
-		self.homeTeam   = []
-		self.awayTeam   = []
-		self.homeFT     = []
-		self.awayFT     = []
-		self.prediction = []
-		self.threshold  = []
-		self.bestOdds   = []
-		self.worstOdds  = []		
+		self.resultList.append(result)
+		self.dateList.append(datetime.datetime.strptime(date, '%d/%m/%y'))
+		self.homeTeam.append(homeTeam)
+		self.awayTeam.append(awayTeam)
+		self.homeFT.append(homeFT)
+		self.awayFT.append(awayFT)
+		self.prediction.append(prediction)
+		self.threshold.append(threshold[1])
+		self.configThreshold.append(configThreshold)
+		self.bestOdds.append(bestOdds)
+		self.worstOdds.append(worstOdds)
 
-	def checkNextDate(self, dateStr):
+
+	def add(self, result, date, homeTeam, awayTeam, homeFT, awayFT, prediction, threshold, configThreshold, bestOdds, worstOdds):
+		""" Add more bet details, essentially, add more bets to an 
+			accumulator. 
+		"""		
+		self.resultList.append(result)
+		self.dateList.append(datetime.datetime.strptime(date, '%d/%m/%y'))
+		self.homeTeam.append(homeTeam)
+		self.awayTeam.append(awayTeam)
+		self.homeFT.append(homeFT)
+		self.awayFT.append(awayFT)
+		self.prediction.append(prediction)
+		self.threshold.append(threshold[1])
+		self.configThreshold.append(configThreshold)
+		self.bestOdds.append(bestOdds)
+		self.worstOdds.append(worstOdds)	
+
+	def checkDate(self, dateStr):
 		date = datetime.datetime.strptime(dateStr, '%d/%m/%y')
-		if len(resultList) == 0:
+		if len(self.resultList) == 0:
 			return True
 		else:
-			prevDate = dateList[-1]
+			prevDate = self.dateList[-1]
 			if date < prevDate :
 				return False
 			else:
@@ -74,3 +70,68 @@ class Bet:
 				else:
 					return False
 
+	def printBet(self):
+		wonTotal = 0
+		lossTotal = 0
+		wonAllBets = True
+		bestTotal = 0.0
+		worstTotal = 0.0
+		startDate = self.dateList[0].strftime('%d/%m/%Y')
+		endDate = self.dateList[-1].strftime('%d/%m/%Y')
+	
+		for index, result in enumerate(self.resultList):
+			betThreshold = self.threshold[index]
+			betThresholdLimit = self.configThreshold[index]
+			bestOdds = self.bestOdds[index]
+			worstOdds = self.worstOdds[index]
+			
+			if bestOdds == 0.0 or worstOdds == 0.0:
+				print self.homeTeam[index],self.awayTeam[index],bestOdds, worstOdds
+			#print betThreshold, type(betThreshold)
+			#print betThresholdLimit, type(betThresholdLimit)
+			
+			if betThreshold >= betThresholdLimit:
+				bestTotal += bestOdds
+				worstTotal += worstOdds
+				if result < 1.0:
+					lossTotal += 1
+					wonAllBets = False
+				else:
+					wonTotal += 1
+					
+		if wonAllBets and lossTotal == 0 and wonTotal > 0:
+			print 'WON BET {0:.2f},{1:.2f},{2},{3},{4},{5},{6}'.format(bestTotal, 
+				   worstTotal, (wonTotal+lossTotal), startDate, endDate, wonTotal, lossTotal)
+		elif not wonAllBets and lossTotal > 0:
+			print 'LOST BET {0:.2f},{1:.2f},{2},{3},{4},{5},{6}'.format(bestTotal, 
+				   worstTotal, (wonTotal+lossTotal), startDate, endDate, wonTotal, lossTotal)
+				   
+
+	def getOdds(self):
+		wonTotal = 0
+		lossTotal = 0
+		wonAllBets = True
+		bestTotal = 0.0
+		worstTotal = 0.0
+		startDate = self.dateList[0].strftime('%d/%m/%Y')
+		endDate = self.dateList[-1].strftime('%d/%m/%Y')
+	
+		for index, result in enumerate(self.resultList):
+			betThreshold = self.threshold[index]
+			betThresholdLimit = self.configThreshold[index]
+			bestOdds = self.bestOdds[index]
+			worstOdds = self.worstOdds[index]
+			
+			if bestOdds == 0.0 or worstOdds == 0.0:
+				print self.homeTeam[index],self.awayTeam[index],bestOdds, worstOdds
+			#print betThreshold, type(betThreshold)
+			#print betThresholdLimit, type(betThresholdLimit)
+			
+			if betThreshold >= betThresholdLimit:
+				bestTotal += bestOdds
+				worstTotal += worstOdds
+				if result < 1.0:
+					lossTotal += 1
+					wonAllBets = False
+				else:
+					wonTotal += 1	
--- leaguepredict/evaluation/ModelEvaluation.py
+++ leaguepredict/evaluation/ModelEvaluation.py
@@ -24,24 +24,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import decomposition
 from leaguepredict.features.FeatureSet import FeatureSet
 from leaguepredict.model.LeaguePredictModel import LeaguePredictModel
+from leaguepredict.evaluation.Bet import Bet
 
 class ModelEvaluation:
 
 
-	def __init__(self, model, featureSet, config): 
+	def __init__(self, model, featureSet, evalutionConfig): 
 		featureList = featureSet.getFeaturesData()
 		header = featureSet.getHeader()		
 		evaluationData = pandas.DataFrame(featureList, columns=header)
 		
-		evalutionConfig = config['evaluation-config']
+		# Get the JSON Config Parameters
 		self.verbose = evalutionConfig['verbose']
 		self.singleThreshold = evalutionConfig['single-threshold']
 		self.accumThreshold = evalutionConfig['accumulator-threshold']
-		
+
 		validCols = featureSet.getValidCols(homeResult=True)
 		self.evaluationResults = evaluationData['homeResult'].tolist()
-
-		self.fixtureDate = evaluationData['date'].tolist()
+		self.fixtureDates = evaluationData['date'].tolist()
 		self.homeTeam = evaluationData['homeTeam'].tolist()
 		self.awayTeam = evaluationData['awayTeam'].tolist()
 		self.homeFT = evaluationData['homeFT'].tolist()
@@ -52,48 +52,36 @@ class ModelEvaluation:
 		self.worstOddsList = evaluationData['worstHomeOdds'].tolist()
 		self.bestOddsList = evaluationData['bestHomeOdds'].tolist()
 
+		self.singleBetsList = []
+		self.accumBetsList = []
+
+
 	def printSummary(self):
-		""" Print the summary for the evaluation metrics.
+		""" Calculates the accumulator and single bets for the predictions. 
+			The gain and the total are dependent on the config inputs. 
 		"""
-		totalWins      = [0.0, 0.0]
-		totalLosses    = [0.0, 0.0]
-		totalIgnored   = [0.0, 0.0]
-		totalBestGain  = [0.0, 0.0]
-		totalWorstGain = [0.0, 0.0]
-		totalBets      = [0.0, 0.0]
-	
-		# The previous date, initialized to an early date
-		prevDate = datetime.datetime(1990,1,1,0,0,0)
-		
 		for index, result in enumerate(self.evaluationResults):
+			result = self.evaluationResults[index]
+			fixtureDate = self.fixtureDates[index]
 			prediction = self.predictions[index]
 			threshold = self.thresholds[index]
-
-			fixtureDate = datetime.datetime.strptime(self.fixtureDate[index], '%d/%m/%y')
-			
-
-
 			homeTeam = self.homeTeam[index]
 			awayTeam = self.awayTeam[index]
 			homeFT = self.homeFT[index]
 			awayFT = self.awayFT[index]
-
 			bestOdds = self.bestOddsList[index]
 			worstOdds = self.worstOddsList[index]
-
-			if threshold[1] >= self.singleThreshold and result > 0.0:
-				totalWins[0] += 1.0
-				totalBestGain[0]  += (bestOdds-1.0)
-				totalWorstGain[0] += (worstOdds-1.0)
-				totalBets[0] += 1.0
-			elif threshold[1] > self.singleThreshold and result < 1.0:
-				totalLosses[0]   += 1.0
-				totalBestGain[0]  -= 1.0
-				totalWorstGain[0] -= 1.0
-				totalBets[0] += 1.0
-			else:
-				totalIgnored  += 1.0
-	
-		print 'Bets:{0}, Won:{1}, Lost:{2}, Ignored:{3}, BestGain:{4:.2f}, WorstGain:{5:.2f}'.format(totalBets[0], \
-			   totalWins[0], totalLosses[0], totalIgnored[0], totalBestGain[0], totalWorstGain[0])
-
+			
+			addedBet = False
+			for bet in self.accumBetsList:
+				if bet.checkDate(fixtureDate):
+					bet.add(result, fixtureDate, homeTeam, awayTeam, homeFT, awayFT, prediction, threshold, self.accumThreshold, bestOdds, worstOdds)
+					addedBet = True
+					break
+			
+			if not addedBet:
+				bet = Bet(result, fixtureDate, homeTeam, awayTeam, homeFT, awayFT, prediction, threshold, self.accumThreshold, bestOdds, worstOdds)
+				self.accumBetsList.append(bet)
+		
+		for bet in self.accumBetsList:
+			bet.printBet()
